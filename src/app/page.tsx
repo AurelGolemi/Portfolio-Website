ded98a10b5bc2962b1d3e9d5c250e0bf30a1837d
Aurel Golemi <golemiaurel68@gmail.com>
2025-09-30 22:32:45 +0300

    Fixed Hire Me Button to use Contact Modal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -62,7 +62,7 @@ export default function Home() {
                 GitHub
               </a>
               <a
-                href="https://mail.google.com/mail/u/0/#inbox?compose=CllgCJTLGkQPXNnVddnNxZMrBkbSKLWXMTGXQfcRVmMkzTmHddtNCSMXcPhbNFpzlDSSVFRLRPL"
+                onClick={() => setIsContactModalOpen(true)}
                 className="bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-800 transition-colors cursor-pointer"
                 target="_blank"
               >
